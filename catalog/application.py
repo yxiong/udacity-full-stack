e5635b950ea795680f063cb3658d4decdb0b34ae
Ying Xiong <yxiong@seas.harvard.edu>
2015-05-13 16:46:12 -0400

    Move some template into .py file

--- catalog/application.py
+++ catalog/application.py
@@ -30,11 +30,40 @@ categories = []
 items = []
 
 
-@app.route('/')
+def render_category_link(category):
+    return '<a href="#" class="list-group-item">{0}</a>'.format(category.name)
+
+
+def render_item_preview_div(item):
+    template = (
+        u'<div class="col-xs-6 col-lg-4">\n'
+        '  <h2>{0}</h2>\n'
+        '  <p>{1}</p>\n'
+        '  <p><a class="btn btn-default" href="#" role="button">'
+        'View details &raquo;</a></p>\n'
+        '</div><!--/.col-xs-6.col-lg-4-->'
+    )
+    return template.format(item.name, item.short_description(200))
+
+
+@app.route("/")
 def IndexHandler():
-    category = categories[0]
-    template = jinjaEnv.get_template("index.html")
-    return template.render(categories = categories, items = items)
+    template = jinjaEnv.get_template("category.html")
+    category_links = [render_category_link(c) for c in categories]
+    item_divs = [render_item_preview_div(i) for i in items]
+    return template.render(item_divs = item_divs,
+                           category_links = category_links)
+
+
+@app.route("/<category>")
+def CategoryHandler(category):
+    return "Handling: " + category
+
+
+@app.route("/<category>/<item>")
+def ItemHandler(category, item):
+    return "Handling %s in %s" % (category, item)
+
 
 if __name__ == "__main__":
     categories = session.query(Category).all()
