a8e89f6189e2d41865c273f22a49429e8ce80ddc
Ying Xiong <yxiong@seas.harvard.edu>
2015-05-14 14:56:50 -0400

    Update category and item

--- catalog/application.py
+++ catalog/application.py
@@ -4,17 +4,19 @@
 # Created: May 11, 2015.
 
 from database_setup import Base, Category, Item
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect, flash
 import os
 import os.path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
+app.secret_key = "secret"  # TODO
 
 
 # Database configuration.
-engine = create_engine("sqlite:///catalog.db")
+engine = create_engine("sqlite:///catalog.db",
+                       connect_args = {"check_same_thread":False}) # TODO
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
@@ -71,14 +73,81 @@ def read_item(category_name, item_name):
                            category_links = category_links)
 
 
-@app.route("/u/<category_name>")
+@app.route("/u/<category_name>", methods=["GET"])
 def update_category(category_name):
-    return render_template("edit.html")
+    category = categories[category_name]
+    cancel_url = url_for('read_category', category_name = category_name)
+    return render_template("edit.html",
+                           title = "Edit a category.",
+                           entity = category,
+                           cancel_url = cancel_url)
+
+
+@app.route("/u/<category_name>", methods=["POST"])
+def update_category_post(category_name):
+    category = categories[category_name]
+    old_name = category.name
+    new_name = request.form["name"]
+    if old_name != new_name:
+        if new_name in categories:
+            flash("The category name '{0}' already exists.".format(new_name))
+            return redirect(url_for('update_category',
+                                    category_name = old_name))
+
+        items[new_name] = items[old_name]
+        del items[old_name]
+        for item in items[new_name].values():
+            item.category_name = new_name
+
+        categories[new_name] = categories[old_name]
+        del categories[old_name]
+        category.name = new_name
+
+    category.description = request.form["description"]
+    category.wiki_url = request.form["wiki"]
+    session.add(category)
+    session.commit()
+    flash("The category has been updated.")
+    return redirect(url_for('read_category', category_name = new_name))
 
 
 @app.route("/u/<category_name>/<item_name>")
 def update_item(category_name, item_name):
-    return render_template("edit.html")
+    item = items[category_name][item_name]
+    cancel_url = url_for('read_item',
+                         category_name = category_name,
+                         item_name = item_name)
+    return render_template("edit.html",
+                           title = "Edit an item.",
+                           entity = item,
+                           cancel_url = cancel_url)
+
+
+@app.route("/u/<category_name>/<item_name>", methods=["POST"])
+def update_item_post(category_name, item_name):
+    category = categories[category_name]
+    item = items[category_name][item_name]
+    old_name = item.name
+    new_name = request.form["name"]
+    if old_name != new_name:
+        if new_name in items[category_name]:
+            flash("The item name '{0}' already exists.".format(new_name))
+            return redirect(url_for('update_item',
+                                    category_name = category_name,
+                                    item_name = item_name))
+
+        items[category_name][new_name] = items[category_name][old_name]
+        del items[category_name][old_name]
+        item.name = new_name
+
+    item.description = request.form["description"]
+    item.wiki_url = request.form["wiki"]
+    session.add(item)
+    session.commit()
+    flash("The item has been updated.")
+    return redirect(url_for('read_item',
+                            category_name = category_name,
+                            item_name = new_name))
 
 
 if __name__ == "__main__":
@@ -90,5 +159,5 @@ if __name__ == "__main__":
         item.category_name = cname
         items.setdefault(cname, {})[item.name] = item
 
-    app.debug = True
+    app.debug = True  # TODO
     app.run(host = "0.0.0.0", port = 8000)
