18f99e0d4c0a3382249e28f4c05bc0344e543f69
Ying Xiong <yxiong@seas.harvard.edu>
2015-05-19 15:55:50 -0400

    Update with project id and client id

--- conference/settings.py
+++ conference/settings.py
@@ -12,7 +12,7 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '863466117284-7f7jqgqsu77p9im7qr5ulqmg04bk91vs.apps.googleusercontent.com'
 ANDROID_CLIENT_ID = 'replace with Android client ID'
 IOS_CLIENT_ID = 'replace with iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
--- conference/static/js/app.js
+++ conference/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '863466117284-7f7jqgqsu77p9im7qr5ulqmg04bk91vs.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
