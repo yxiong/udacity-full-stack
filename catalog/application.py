664ea80b3686cf600742e4f74aba3e6dd3092dcb
Ying Xiong <yxiong@seas.harvard.edu>
2015-05-26 13:26:40 -0400

    Fix sql multi-thread issue when deploying in debug mode

--- catalog/application.py
+++ catalog/application.py
@@ -458,5 +458,15 @@ def api_json_item(category_name, item_name):
 
 
 if __name__ == "__main__":
+    # When the app is launched in debug mode, sqlite has a multi-thread issue,
+    # which we try to circumvent by re-creating the engine and session with
+    # `check_same_thread=False` argument.
     app.debug = True
+    engine = create_engine(DATABASE_NAME,
+                           connect_args={"check_same_thread": False})
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind = engine)
+    db_session = DBSession()
+
+    # Launch the application.
     app.run(host = "0.0.0.0", port = 8000)
